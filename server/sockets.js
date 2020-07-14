6a59c4f528c9787708b77af6169acc6bd1d8426a
Kim Hyunse / Arthur <anzmf12@naver.com>
2020-07-14 19:42:58 -0400

    add server socket listner

--- server/sockets.js
+++ server/sockets.js
@@ -7,22 +7,22 @@ sockets.init = function (server) {
   // Create Match Manager
   const Match = new MatchManager();
   // socket.io setup
-  var io = require("socket.io")(server, {
+  var io = require('socket.io')(server, {
     pingInterval: 5000,
     pingTimeout: 10000,
   });
 
-  io.on("connect", (socket) => {
-    console.log("connected", socket.id, new Date().toLocaleTimeString());
-    socket.on("disconnect", () => {
-      console.log("disconnected", socket.id, new Date().toLocaleTimeString());
+  io.on('connect', (socket) => {
+    console.log('connected', socket.id, new Date().toLocaleTimeString());
+    socket.on('disconnect', () => {
+      console.log('disconnected', socket.id, new Date().toLocaleTimeString());
     });
 
     //joining the game (room)
-    socket.on("BE-user-joined", async ({ gameId, player }) => {
+    socket.on('BE-user-joined', async ({ gameId, player }) => {
       const newPlayer = new Player(player.email, player.name);
 
-      if(!Match.checkRoomExist(gameId)) {
+      if (!Match.checkRoomExist(gameId)) {
         Match.createRoom(gameId);
       }
 
@@ -32,18 +32,50 @@ sockets.init = function (server) {
       //joining room
       socket.join(gameId);
       //notify all other users in the room
-      socket.broadcast.to(gameId).emit("FE-user-joined", player.email);
+      socket.broadcast.to(gameId).emit('FE-user-joined', player.email);
     });
 
     //host starts the game
-    socket.on("start-game", (gameId) => {
+    socket.on('start-game', (gameId) => {
       // Start Game
       const gameState = Match.startGame(gameId);
 
-      socket.broadcast.to(gameId).emit("game-started", gameState);
+      console.log(gameState);
+
+      socket.broadcast.to(gameId).emit('game-started', gameState);
+    });
+
+    // Send Clues
+    socket.on('BE-send-clue', ({ gameId, player }) => {
+      console.log(`Clue is ${player.msg} from ${player.name}`);
+
+      io.sockets.in(gameId).emit('FE-send-clue', player);
+    });
+
+    // Send Answer 
+    socket.on('BE-send-answer', (gameId) => {
+      const gameState = Match.endRound(roomName, answer, clues);
+    
+      console.log("End Round : ", gameState.state.players);
+
+      socket.broadcast.to(gameId).emit('FE-send-answer', gameState);
+    });
+
+    // Next Round
+    socket.on('BE-move-round', (gameId) => {
+      const gameState = Match.moveToNextRound(gameId);
+  
+      io.sockets.in(gameId).emit('FE-move-round', gameState);
+    });
+
+    // Reset game
+    socket.on('BE-reset-game', (gameId) => {
+      const gameState = Match.startGame(gameId);
+  
+      io.sockets.in(gameId).emit('FE-reset-game', gameState);
     });
   });
-  console.log("Sockets Initialized");
+  console.log('Sockets Initialized');
 };
 
 module.exports = sockets;
